e926e756ae15d9f18f95f6367c82d0a74f30879e
strangelavita <mohitkum619@gmail.com>
2021-06-15 11:28:11 +0530

    initialisation

---
+++ src/main/java/lavita/strange/petcare/PetCareApplication.java
@@ -0,0 +1,13 @@
+package lavita.strange.petcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lavita/strange/petcare/PetCareApplicationTests.java
@@ -0,0 +1,13 @@
+package lavita.strange.petcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
